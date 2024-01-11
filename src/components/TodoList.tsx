4378cc87d2fa9c08ec737a4f41b2d3abde187858
aribxax <aribxax@gmail.com>
2024-01-11 16:10:06 +0100

    2nd

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,19 +1,36 @@
-import React, { MouseEvent } from 'react';
+import React, { FormEvent, useState, useEffect } from 'react';
 
 
-    const handleMouseEvent = (e: MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        console.log(e);
-    }
 
 
 function TodoList() {
+
+
+    const [ newItem, setNewItem ] = useState() 
+
+    useEffect(() => {
+    
+    }, newItem)
+
+    const handleFormEvent = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        console.log('holaa')
+        console.log(newItem)
+        console.log('nadaa')
+    };
+
+    const inputChange = (e: React.SetStateAction<string> ) => {
+        setNewItem(e)
+    }
+
+
     return (
         <>
             
-            <form>
-                    <input type='text' placeholder='New item'></input>
-                    <button type='submit' onClick={e => handleMouseEvent(e)}>Add</button>
+            <form onSubmit={handleFormEvent}>
+                    <input type='text' placeholder='New item' onChange={e => inputChange(e)}></input>
+                    <button type='submit'>Add</button>
             </form>
             <div style={{backgroundColor:'grey',height:'40vh', width:'40vh'}}>
 
