f3fb1d4a1347d311e8e0779b31326d67860ea35b
aribxax <aribxax@gmail.com>
2024-01-12 17:12:16 +0100

    cambio lista

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,27 +1,24 @@
-import React, { FormEvent, useState, useEffect } from 'react';
+/* import React, { FormEvent, useState, useEffect } from 'react';
 
 
+interface Item {
+   
+}
+
+const TodoList: React.FC<Item> = () => {
 
 
-function TodoList() {
+    const [ newItem, setNewItem ] = useState<string>('') 
 
 
-    const [ newItem, setNewItem ] = useState() 
-
-    useEffect(() => {
-    
-    }, newItem)
 
     const handleFormEvent = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-
-        console.log('holaa')
         console.log(newItem)
-        console.log('nadaa')
     };
 
-    const inputChange = (e: React.SetStateAction<string> ) => {
-        setNewItem(e)
+    const inputChange = (e: Item) => {
+        setNewItem(`${Item}`)
     }
 
 
@@ -29,14 +26,46 @@ function TodoList() {
         <>
             
             <form onSubmit={handleFormEvent}>
-                    <input type='text' placeholder='New item' onChange={e => inputChange(e)}></input>
+                    <input type='text' placeholder='New item' onChange={inputChange}></input>
                     <button type='submit'>Add</button>
             </form>
             <div style={{backgroundColor:'grey',height:'40vh', width:'40vh'}}>
 
-            </div>
+            </div> 
         </>
     );
 }
 
-export default TodoList;
+export default TodoList; */
+
+
+
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+
+interface MyComponentProps {
+  // Define your component props here
+}
+
+const MyComponent: React.FC<MyComponentProps> = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const inputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleFormEvent = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Use the inputValue in your logic
+    console.log('Input Value:', inputValue);
+    // Add further logic here
+  };
+
+  return (
+    <form onSubmit={handleFormEvent}>
+      <input type='text' placeholder='New item' value={inputValue} onChange={inputChange} />
+      <button type='submit'>Add</button>
+    </form>
+  );
+};
+
+export default MyComponent; 
